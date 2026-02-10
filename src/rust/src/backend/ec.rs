a9eaf3d917380eb14e79644de4cfdeaace9ac4f1
Paul Kehrer <paul.l.kehrer@gmail.com>
2026-02-10 08:59:24 -0600

    Run an EC check key if cofactor > 1
    This only applies to the binary curves (ed25519 is cofactor 8 and
    ed448 is cofactor 4 but we use a different code path for eddsa)

--- src/rust/src/backend/ec.rs
+++ src/rust/src/backend/ec.rs
@@ -109,6 +109,14 @@ fn check_key_infinity(
     Ok(())
 }
 
+fn cofactor_check(group: &openssl::ec::EcGroupRef) -> CryptographyResult<bool> {
+    let mut bn_ctx = openssl::bn::BigNumContext::new()?;
+    let mut cofactor = openssl::bn::BigNum::new()?;
+    group.cofactor(&mut cofactor, &mut bn_ctx)?;
+    let one = openssl::bn::BigNum::from_u32(1)?;
+    Ok(cofactor.ucmp(&one) == std::cmp::Ordering::Greater)
+}
+
 #[pyo3::pyfunction]
 fn curve_supported(py: pyo3::Python<'_>, py_curve: pyo3::Bound<'_, pyo3::PyAny>) -> bool {
     curve_from_py_curve(py, py_curve).is_ok()
@@ -384,6 +392,13 @@ impl ECPublicKey {
     ) -> CryptographyResult<ECPublicKey> {
         let ec = pkey.ec_key()?;
         check_key_infinity(&ec)?;
+        if cofactor_check(ec.group())? {
+            ec.check_key().map_err(|_| {
+                pyo3::exceptions::PyValueError::new_err(
+                    "Invalid EC key (key out of range, infinity, etc.)",
+                )
+            })?;
+        }
 
         Ok(ECPublicKey { pkey, curve })
     }
